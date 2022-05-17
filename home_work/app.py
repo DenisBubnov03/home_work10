9afcf79d7523d278906314455d01a4854638ec24
denisbubnov03 <denisbu2000@gmail.com>
2022-05-17 20:58:09 +0300

    Fixed display if no candidates, add search by id.

--- home_work/app.py
+++ home_work/app.py
@@ -18,11 +18,28 @@ def main_page():
 @app.route("/candidates/<name>")
 def candidate_page(name):
     for candidate in candidates_list:
-        if name.title().strip() == candidate["name"]:
+        if name.title().strip() in candidate["name"].split(" "):
             return f'<center> <img src="{candidate["picture"]}" width="200" height="200" alt="">\n' \
                    f'<pre>Имя кандидата - {candidate["name"]}\n' \
                    f'Позиция кандидата - {candidate["position"]}\n' \
                    f'Навыки: {candidate["skills"]}</pre>'
+        elif name.title().strip() == candidate["name"]:
+            return f'<center> <img src="{candidate["picture"]}" width="200" height="200" alt="">\n' \
+                   f'<pre>Имя кандидата - {candidate["name"]}\n' \
+                   f'Позиция кандидата - {candidate["position"]}\n' \
+                   f'Навыки: {candidate["skills"]}</pre>'
+    return "Такого кандидата нет в базе"
+
+
+@app.route("/candidates/<id>")
+def candidate_page_id(pk):
+    for candidate in candidates_list:
+        if pk == candidate["id"]:
+            return f'<center> <img src="{candidate["picture"]}" width="200" height="200" alt="">\n' \
+                   f'<pre>Имя кандидата - {candidate["name"]}\n' \
+                   f'Позиция кандидата - {candidate["position"]}\n' \
+                   f'Навыки: {candidate["skills"]}</pre>'
+    return "Такого кандидата нет в базе"
 
 
 @app.route("/skills/<skills>")
@@ -33,6 +50,8 @@ def candidates_page(skills):
             skill += "Имя кандидата - " + candidate["name"] + "\n"
             skill += "Позиция кандидата - " + candidate["position"] + "\n"
             skill += "Навыки: " + candidate["skills"] + "\n"
+    if len(skill) == 0:
+        return "Такого кандидата с умением нет в базе"
     return f"<pre>{skill}</pre>"
 
 
